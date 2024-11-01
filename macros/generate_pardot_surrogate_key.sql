8678e819a90bef1b563acfffe236b642c6729e6f
Tyler Wood <tylercwood@gmail.com>
2024-11-01 13:55:50 -0400

    macro fiddling

--- macros/generate_pardot_surrogate_key.sql
+++ macros/generate_pardot_surrogate_key.sql
@@ -1,3 +1,4 @@
 {% macro generate_pardot_surrogate_key(pre_union_primary_key)%}
-{{ dbt_utils.generate_surrogate_key(['_dbt_source_relation', pre_union_primary_key]) }}
+split_part(_dbt_source_relation, '.', 1) ||'.'|| split_part(_dbt_source_relation, '.', 2) as source_schema,
+{{ dbt_utils.generate_surrogate_key(['source_schema', pre_union_primary_key]) }}
 {% endmacro %}
