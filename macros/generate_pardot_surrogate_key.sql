397988dfd2ece5096cfa25842018b0e4fd77b551
Tyler Wood <tylercwood@gmail.com>
2024-11-01 14:21:26 -0400

    is this the one

--- macros/generate_pardot_surrogate_key.sql
+++ macros/generate_pardot_surrogate_key.sql
@@ -3,7 +3,6 @@
 
     {% set source_schema = "split_part(_dbt_source_relation, '.', 1) || '.' || split_part(_dbt_source_relation, '.', 2)" %}
 
-    {{ source_schema }} as {{ model }}_source_schema,
     {{ dbt_utils.generate_surrogate_key([source_schema, pre_union_primary_key]) }}
 
 {% endmacro %}
